75e427c37b6dc27debab56eba540dacb67a4b005
Eugene Koontz <ekoontz@hiro-tan.org>
2020-12-06 23:07:32 +0100

    parsing input works

--- src/menard_lambda/core.clj
+++ src/menard_lambda/core.clj
@@ -4,16 +4,18 @@
     [fierycod.holy-lambda.core :as h]
     [menard.nederlands :as nl]))
 
-(defn myfunction []
-  (->> (-> "een aardig bedroefd buitengewoon afzonderlijk dik nieuwswaardig ongerust teleurgesteld eigenwijs geheim verrast prachtig slim vrouw probeert mannen te zien"
-           nl/parse)
+(defn myfunction [q]
+  (->> (-> q nl/parse)
        (map menard.nederlands/syntax-tree)
        (clojure.string/join ",")))
 
 (h/deflambda ExampleLambda
   [event context]
-  {:statusCode 200
-   :body (str "Hello world " (myfunction) "!")
-   :isBase64Encoded false})
+  (h/info "Logging...")
+  (h/info (str "THE Q PARAM: " (-> event :queryStringParameters :q)))
+  (let [q (-> event :queryStringParameters :q)]
+    {:statusCode 200
+     :body (str "Hello world " (myfunction q) "!")
+     :isBase64Encoded false}))
 
 (h/gen-main [#'ExampleLambda])
